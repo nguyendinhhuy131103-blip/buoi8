42ea80ecc88daab1dd5a4eec4eb99fddb3a37540
nguyendinhhuy131103-blip <your-email@example.com>
2026-03-23 23:07:12 +0700

    Only keep NNPTUD-S2-20260323 project

--- Downloads/NNPTUD-S2-20260309/NNPTUD-S2-20260309/utils/titleHandler.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    ConvertTitleToSlug: function (title) {
-        let result = title.toLowerCase();
-        result = result.replaceAll(' ', '-');
-        return title;
-    }
-}
